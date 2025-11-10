cb73d4c5fe1037a5acf82946481d5993c1523913
Eric Oliveira <ericoliveirajs@gmail.com>
2025-11-10 11:54:47 -0300

    feat(security): implementa RBAC (Roles Guard) e Admin Role (Issue 19)

--- src/vehicles/vehicles.controller.ts
+++ src/vehicles/vehicles.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Post, Body, Get, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch, Param, Delete, NotFoundException, UseGuards } from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Role } from '../auth/enums/role.enum';
 
 @ApiTags('3. Veículos')
+@UseGuards(RolesGuard)
 @Controller('vehicles')
 export class VehiclesController {
   constructor(private readonly vehiclesService: VehiclesService) {}
 
   @Post()
+  @Roles(Role.Admin)
   create(@Body() createVehicleDto: CreateVehicleDto) {
     return this.vehiclesService.create(createVehicleDto);
   }
@@ -20,6 +25,7 @@ export class VehiclesController {
   }
 
   @Patch(':id')
+  @Roles(Role.Admin)
   async update(
     @Param('id') id: string,
     @Body() updateVehicleDto: UpdateVehicleDto,
@@ -35,6 +41,7 @@ export class VehiclesController {
   }
 
   @Delete(':id')
+  @Roles(Role.Admin)
   async remove(@Param('id') id: string) {
     const deletedVehicle = await this.vehiclesService.remove(id);
     if (!deletedVehicle) {

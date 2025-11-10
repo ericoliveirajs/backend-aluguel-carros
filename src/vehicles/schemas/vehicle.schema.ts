ba00d69721bac1fdb7e94b9aa4598a62233ce891
Eric Oliveira <ericoliveirajs@gmail.com>
2025-11-10 08:42:46 -0300

    feat(vehicle): implementa cadastro de veículo (Issue 8)

---
+++ src/vehicles/schemas/vehicle.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type VehicleDocument = HydratedDocument<Vehicle>;
+
+export enum VehicleStatus {
+  DISPONIVEL = 'disponivel',
+  RESERVADO = 'reservado',
+}
+
+@Schema({ timestamps: true })
+export class Vehicle {
+
+  @Prop({ required: true })
+  model: string;
+
+  @Prop({ required: true })
+  brand: string;
+
+  @Prop({ required: true })
+  type: string;
+
+  @Prop({ required: true })
+  motor: number;
+
+  @Prop({ required: true })
+  luggageCapacity: number;
+
+  @Prop({ required: true, enum: VehicleStatus, default: VehicleStatus.DISPONIVEL })
+  status: VehicleStatus;
+}
+
+export const VehicleSchema = SchemaFactory.createForClass(Vehicle);
--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
-import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { Vehicle, VehicleDocument } from './schemas/vehicle.schema';
 
 @Injectable()
 export class VehiclesService {
-  create(createVehicleDto: CreateVehicleDto) {
-    return 'This action adds a new vehicle';
-  }
+  constructor(
+    @InjectModel(Vehicle.name) private vehicleModel: Model<VehicleDocument>,
+  ) {}
 
-  findAll() {
-    return `This action returns all vehicles`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} vehicle`;
-  }
-
-  update(id: number, updateVehicleDto: UpdateVehicleDto) {
-    return `This action updates a #${id} vehicle`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} vehicle`;
+  async create(createVehicleDto: CreateVehicleDto): Promise<VehicleDocument> {
+    const createdVehicle = new this.vehicleModel(createVehicleDto);
+    return createdVehicle.save();
   }
 }

a1ec177be08171a8f1c448a9cd9dd7de60d163d4
Eric Oliveira <ericoliveirajs@gmail.com>
2025-11-10 10:20:46 -0300

    feat(user): implementa rota de edição de usuário (Issue 7)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, Put, Param, Body, ForbiddenException, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiTags } from '@nestjs/swagger';
+import { UpdateUserDto } from './dto/update-user.dto';
+import type { Request } from 'express';
 
 @ApiTags('2. Usuários')
 @Controller('users')
@@ -10,5 +12,29 @@ export class UsersController {
   @Get('me')
   getProfile(@Req() req) {
     return req.user;
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Req() req: Request,
+  ) {
+    const loggedInUserId = (req.user as any)._id;
+
+    if (loggedInUserId.toString() !== id) {
+      throw new ForbiddenException(
+        'Você não tem permissão para editar este usuário.',
+      );
+    }
+
+    const updatedUser = await this.usersService.update(id, updateUserDto);
+
+    if (!updatedUser) {
+      throw new NotFoundException('Usuário não encontrado para atualização.');
+    }
+
+    const { password, ...result } = updatedUser.toObject();
+    return result;
   }
 }
